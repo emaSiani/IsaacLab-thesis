98137e5f0a82eda5adaf02fba8e0c136e391e888
Emanuele Giuseppe Siani <emanuele.giuseppe.siani@external.toyota-europe.com>
2026-01-27 17:50:27 +0100

    Deployed on real robot on 27th January

--- scripts/sim2real/robots/rizon/assembly.py
+++ scripts/sim2real/robots/rizon/assembly.py
@@ -5,16 +5,20 @@ from scipy.spatial.transform import Rotation as R
 
 DEBUG = True
 
+# INITIAL ROBOT POSE
+# deg: [-7,44, -32,7, 24,17, 102,3, -18,16, 40,4, 0.0]  
+
+
 class FlexivGearAssemblyPolicy:
     def __init__(self):
         # --- PATH CONFIG ---
         #self.policy_path = r"robots/rizon/policies/rizon4s_200ep_512envs_increase_kp_scale.pt"
         self.policy_path = r"robots/rizon/policies/rizon4s_200ep_512envs_fixed_midpoint_policy.pt"
-        
+
         # [CRITICAL] Posizione del FIXED ASSET (Bullone) nel frame del robot reale.
         self.fixed_pos = np.array([0.6047, 0.02619, 0.0782]) 
         self.fixed_pos[0] += 0.02025  # offset of the bolt
-        
+
         self.force_threshold = np.array([1.1]) 
 
         # --- MODEL LOAD ---
@@ -26,18 +30,18 @@ class FlexivGearAssemblyPolicy:
         # --- PARAMETRI TRAINING ---
         self.dt = 1.0 / 15.0 # ~0.066s (corretto rispetto a 0.02s di simulazione fisica pura)
         # Nota: self.dt qui è il dt di CONTROLLO (decimato). 
-        
+
         # Bounds & Thresholds
         self.pos_action_bounds = np.array([0.05, 0.05, 0.05], dtype=np.float32)
         self.rot_action_bounds = np.array([1.0, 1.0, 1.0], dtype=np.float32)
-        
+
         # Clipping
         self.pos_threshold = 0.02
         self.rot_threshold = 0.097
 
         # EMA Smoothing (Azioni)
         self.ema_factor = 0.05
-        
+
         # FT Smoothing (Forza) - Dal config originale ft_smoothing_factor = 0.25
         self.ft_smoothing_factor = 0.25
 
@@ -46,7 +50,7 @@ class FlexivGearAssemblyPolicy:
         self.prev_ee_quat = None 
         self.prev_action = np.zeros(7, dtype=np.float32) 
         self.prev_action_smooth = np.zeros(7, dtype=np.float32)
-        
+
         # Buffer per smoothing forza
         self.force_sensor_world_smooth = np.zeros(3, dtype=np.float32)
 
@@ -83,10 +87,10 @@ class FlexivGearAssemblyPolicy:
 
         # 2. Compute Input Velocities (Finite Difference)
         lin_vel = (current_ee_pos - self.prev_ee_pos) / self.dt
-        
+
         if np.dot(current_ee_quat, self.prev_ee_quat) < 0:
             current_ee_quat = -current_ee_quat 
-            
+
         r_curr = R.from_quat([current_ee_quat[1], current_ee_quat[2], current_ee_quat[3], current_ee_quat[0]])
         r_prev = R.from_quat([self.prev_ee_quat[1], self.prev_ee_quat[2], self.prev_ee_quat[3], self.prev_ee_quat[0]])
         r_diff = r_curr * r_prev.inv()
@@ -95,11 +99,11 @@ class FlexivGearAssemblyPolicy:
 
         # 3. FORCE PROCESSING (Solo Smoothing, NIENTE Rotazione)
         # Il topic ROS ext_wrench_in_world è già orientato correttamente.
-        
+
         # Applica Smoothing esponenziale
         alpha = self.ft_smoothing_factor
         self.force_sensor_world_smooth = alpha * current_force_world_raw + (1 - alpha) * self.force_sensor_world_smooth
-        
+
         current_force_obs = self.force_sensor_world_smooth
 
         # 4. Posizione Relativa
@@ -127,7 +131,7 @@ class FlexivGearAssemblyPolicy:
             print(f"\n##################### Step: {self.step_counter} OBSERVATION #####################")
             keys = ["pos_rel", "quat", "lin_vel", "ang_vel", "force_smooth", "threshold", "prev_act"]
             vals = [pos_rel, current_ee_quat, lin_vel, ang_vel, current_force_obs, self.force_threshold, masked_prev_actions]
-            
+
             for key, val in zip(keys, vals):
                 print(f"{key:<15}: {np.array2string(val, precision=4, suppress_small=True)}")
             print("#####################################################################\n")
@@ -160,19 +164,20 @@ class FlexivGearAssemblyPolicy:
         # 9. FORGE LOGIC: Convert Action to Twist
         pos_action_delta = smooth_action[0:3] * self.pos_action_bounds
         rot_action_delta = smooth_action[3:6] * self.rot_action_bounds
-        
+
         target_pos_world = self.fixed_pos + pos_action_delta
         delta_pos = target_pos_world - current_ee_pos
         delta_pos_clipped = np.clip(delta_pos, -self.pos_threshold, self.pos_threshold)
-        
+
         v_lin_cmd = delta_pos_clipped / self.dt
-        
+
         delta_rot_clipped = np.clip(rot_action_delta, -self.rot_threshold, self.rot_threshold)
         v_ang_cmd = delta_rot_clipped / self.dt 
 
         self.prev_ee_pos = current_ee_pos
         self.prev_ee_quat = current_ee_quat
-        
+
         self.step_counter += 1
-        
+
         return np.concatenate([v_lin_cmd, v_ang_cmd]), success_score
+ 
--- scripts/sim2real/run_assembly_task.py
+++ scripts/sim2real/run_assembly_task.py
@@ -25,21 +25,21 @@ from robots.rizon.assembly import FlexivGearAssemblyPolicy
 
 # --- CONFIGURAZIONE ---
 URDF_PATH = "robots/rizon4s_kinematics.urdf" 
-CONTROL_FREQ = 15.0 
+CONTROL_FREQ = 60.0 
 DEBUG = True
 SUCCESS_THRESHOLD = 0.9995
 #SUCCESS_THRESHOLD = 0.98
-serial_number= None
+serial_number= 'Rizon4s-063126'
 
 class FlexivAssemblyNode(Node):
     def __init__(self):
         super().__init__("flexiv_assembly_node")
 
         self.task_completed = False
-        
+
         self.dt = 1.0 / CONTROL_FREQ
         if serial_number is None:
-            self.joint_names_ordered = [f"joint{i}" for i in range(1, 8)]
+           self.joint_names_ordered = [f"joint{i}" for i in range(1, 8)]
         else:
             self.joint_names_ordered = [f"{serial_number}_joint{i}" for i in range(1, 8)]
 
@@ -67,27 +67,30 @@ class FlexivAssemblyNode(Node):
         if not os.path.exists(URDF_PATH):
             self.get_logger().error(f"URDF MANCANTE: {os.path.abspath(URDF_PATH)}")
             raise FileNotFoundError("Manca il file rizon4s_kinematics.urdf")
-            
+
         self.model = pin.buildModelFromUrdf(URDF_PATH)
         self.data = self.model.createData()
         self.frame_id = self.model.getFrameId("flange")
-        
+
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             history=HistoryPolicy.KEEP_LAST,
             depth=1
         )
 
-        self.sub_joints = self.create_subscription(JointState, "/joint_states", self.cb_joints, qos_profile)
-        
-        if FLEXIV_IMPORTED:
-            self.sub_states = self.create_subscription(RobotStates, "/flexiv_robot_states", self.cb_states, qos_profile)
-        
+        #self.sub_joints = self.create_subscription(JointState, "/joint_states", self.cb_joints, qos_profile)
+        self.sub_joints = self.create_subscription(JointState, "flexiv_arm/joint_states", self.cb_joints, qos_profile)
+
+        if FLEXIV_IMPORTED and serial_number is None:
+                self.sub_states = self.create_subscription(RobotStates, "/flexiv_robot_states", self.cb_states, qos_profile)
+        elif FLEXIV_IMPORTED and serial_number:
+                self.sub_states = self.create_subscription(RobotStates, f"{serial_number.replace('-','_')}/flexiv_robot_states", self.cb_states, qos_profile)
+
         # Sottoscrizione Wrench diretto (Backup per Sim)
-        # if serial_number is None:
-        #     self.sub_wrench = self.create_subscription(WrenchStamped, "/sn/external_wrench_in_world", self.cb_wrench_direct, qos_profile)
-        # else:
-        #     self.sub_wrench = self.create_subscription(WrenchStamped, f"/{serial_number}/external_wrench_in_world", self.cb_wrench_direct, qos_profile)
+        if serial_number is None:
+            self.sub_wrench = self.create_subscription(WrenchStamped, "/sn/external_wrench_in_world", self.cb_wrench_direct, qos_profile)
+        else:
+            self.sub_wrench = self.create_subscription(WrenchStamped, f"{serial_number.replace('-','_')}/external_wrench_in_world", self.cb_wrench_direct, qos_profile)
         self.last_wrench_msg = None
 
         self.pub_traj = self.create_publisher(JointTrajectory, "/rizon_arm_controller/joint_trajectory", 1)
@@ -96,9 +99,13 @@ class FlexivAssemblyNode(Node):
         self.get_logger().info(f"✅ Nodo Avviato. In attesa di TARE ({self.tare_steps} steps)...")
 
     def cb_states(self, msg):
+        # self.get_logger().info(f"✅ Received message({msg}")
+
         self.robot_state = msg
 
     def cb_wrench_direct(self, msg):
+        # self.get_logger().info(f"✅ Received wrench")
+
         self.last_wrench_msg = msg
 
     def cb_joints(self, msg):
@@ -117,12 +124,13 @@ class FlexivAssemblyNode(Node):
         # 1. Check Data Availability
         pose_available = False
         wrench_available = False
-        
+
         curr_pos = np.zeros(3)
         curr_quat = np.array([1,0,0,0]) # wxyz
         curr_wrench = np.zeros(3)
 
         if self.robot_state is not None:
+            # self.get_logger().info(f"✅ Robot state is available")
             pose_available = True
             wrench_available = True
             curr_pos = np.array([
@@ -132,15 +140,16 @@ class FlexivAssemblyNode(Node):
             ])
             q_msg = self.robot_state.tcp_pose.pose.orientation
             curr_quat = np.array([q_msg.w, q_msg.x, q_msg.y, q_msg.z])
-            
+
             curr_wrench = np.array([
                 self.robot_state.ext_wrench_in_world.wrench.force.x,
                 self.robot_state.ext_wrench_in_world.wrench.force.y,
                 self.robot_state.ext_wrench_in_world.wrench.force.z
             ])
-        
+
         # Fallback se usi lo script Isaac che pubblica WrenchStamped separatamente
         if not wrench_available and self.last_wrench_msg is not None:
+            # self.get_logger().info(f"✅ Qui ci sono pure")
             wrench_available = True
             curr_wrench = np.array([
                 self.last_wrench_msg.wrench.force.x,
@@ -178,12 +187,12 @@ class FlexivAssemblyNode(Node):
         # --- 4. DEBUG (CON EMOJI) ---
         if DEBUG and (self.step_count % 100 == 0 or self.step_count < 3):
             dist = np.linalg.norm(curr_pos - self.policy.fixed_pos)
-            
+
             # Formattazione stringhe per allineamento
             p_str = np.array2string(curr_pos, precision=4, suppress_small=True)
             w_str = np.array2string(wrench_cleaned, precision=2, suppress_small=True)
             t_str = np.array2string(target_twist[:3], precision=4, suppress_small=True)
-            
+
             print(f"\n--- 🐛 DEBUG STEP {self.step_count} ---")
             print(f"📍 TCP Current:  {p_str}")
             print(f"📏 Dist to Goal: {dist:.4f} m")
@@ -213,10 +222,10 @@ class FlexivAssemblyNode(Node):
         dls_lambda = 0.05
         J_T = J.T
         J_pinv = J_T @ np.linalg.inv(J @ J_T + dls_lambda**2 * np.eye(6))
-        
+
         q_dot = J_pinv @ target_twist
         q_cmd = self.current_q + q_dot * self.dt
-        
+
         max_q_step = 0.0075
         q_cmd = np.clip(q_cmd, self.current_q - max_q_step, self.current_q + max_q_step)
 
@@ -239,7 +248,7 @@ class FlexivAssemblyNode(Node):
             return
 
         print("\n📊 Generazione grafici in corso...")
-        
+
         steps = np.array(self.log_steps)
         actions = np.array(self.log_actions) # Shape (N, 6)
         forces = np.array(self.log_forces)   # Shape (N, 3)
@@ -247,7 +256,7 @@ class FlexivAssemblyNode(Node):
 
         # Creazione figura con 3 subplot
         fig, axs = plt.subplots(3, 1, figsize=(10, 12), sharex=True)
-        
+
         # 1. Andamento Azioni (Twist)
         # Plot Linear Velocity
         axs[0].plot(steps, actions[:, 0], label='Vx', linestyle='-', alpha=0.8)
@@ -287,7 +296,7 @@ class FlexivAssemblyNode(Node):
 def main():
     rclpy.init()
     node = FlexivAssemblyNode()
-    
+
     try:
         rclpy.spin(node)
     except (KeyboardInterrupt, SystemExit):
